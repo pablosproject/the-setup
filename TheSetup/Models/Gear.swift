1bc9a1caec8dc89b75ef2d126b9537871a2783d9
Paolo Tagliani <pablosproject@gmail.com>
2017-02-17 17:43:04 +0100

    Created base model for Interview and Gear

---
+++ TheSetup/Models/Gear.swift
@@ -0,0 +1,28 @@
+//
+//  Gear.swift
+//  TheSetup
+//
+//  Created by Paolo Tagliani on 17/02/2017.
+//  Copyright © 2017 Pablosproject. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class Gear:Mappable {
+	public private(set) var slug : String?
+	public private(set) var name : String?
+	public private(set) var url : URL?
+	public private(set) var description : String?
+	
+	required init?(map: Map){
+		
+	}
+	
+	func mapping(map: Map) {
+		slug <- map["slug"]
+		name <- map["name"]
+		url <- map["url"]
+		description <- map["description"]
+	}
+}
